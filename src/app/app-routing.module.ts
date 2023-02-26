3cea34ccaa008552f556f80f8251c4d5b8dc0da1
Sanny Helen Lima <sannyhelenlima@gmail.com>
2023-02-26 12:04:45 -0300

    criação de componentes e definição de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { BiografiaComponent } from './biografia/biografia.component';
+import { BibliotecaComponent } from './biblioteca/biblioteca.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { MusicasComponent } from './musicas/musicas.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'contatos', component: ContatoComponent},
+  {path: 'biografia', component: BiografiaComponent},
+  {path: 'biblioteca', component: BibliotecaComponent},
+  {path: 'agenda', component: AgendaComponent},
+  {path: 'musicas', component: MusicasComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BibliotecaComponent } from './biblioteca/biblioteca.component';
+import { BiografiaComponent } from './biografia/biografia.component';
+import { ContatoComponent } from './contato/contato.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { MenuNavegacaoComponent } from './menu-navegacao/menu-navegacao.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MusicasComponent } from './musicas/musicas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BibliotecaComponent,
+    BiografiaComponent,
+    ContatoComponent,
+    AgendaComponent,
+    MenuNavegacaoComponent,
+    MusicasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
